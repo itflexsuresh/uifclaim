6685b9694a28638ffb99c475b01a54b1c856e14a
manikandanRJSM <manikandanrjsm@outlook.com>
2020-05-22 17:50:26 +0530

    custom modi

--- application/views/common/custom/custom.php
+++ application/views/common/custom/custom.php
@@ -3,7 +3,7 @@
 function customtextbox($count, $name, $values='', $extras=[]){
 	for($i=1; $i<=$count; $i++){
 		$value = ($values!='' && isset($values[$i-1])) ? $values[$i-1] : '';
-		echo '<input maxlength="1" type="text" class="form-control customtextbox" name="'.$name.'[]" value="'.$value.'" data-textbox="textbox1">';
+		echo '<input maxlength="1" type="text" class="form-control customtextbox '.$name.'" name="'.$name.'[]" value="'.$value.'" data-textbox="textbox1" id="'.$i.'_'.$name.'">';
 		if(in_array($i, $extras)) echo '<span class="customtextboxdivider">/</span>';
 	}
 	
